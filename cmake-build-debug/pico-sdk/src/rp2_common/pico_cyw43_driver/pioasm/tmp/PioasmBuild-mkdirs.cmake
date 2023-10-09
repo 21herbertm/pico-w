99c9984b544149c19778b2c8551f969850d95152
21herbertm <53823046+21herbertm@users.noreply.github.com>
2023-10-08 20:42:57 -0400

    worked on i2c and adding cam header files

---
+++ cmake-build-debug/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/tmp/PioasmBuild-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/mherbert/pico-sdk/tools/pioasm"
+  "/Users/mherbert/Desktop/pico-w/cmake-build-debug/pioasm"
+  "/Users/mherbert/Desktop/pico-w/cmake-build-debug/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm"
+  "/Users/mherbert/Desktop/pico-w/cmake-build-debug/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/tmp"
+  "/Users/mherbert/Desktop/pico-w/cmake-build-debug/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src/PioasmBuild-stamp"
+  "/Users/mherbert/Desktop/pico-w/cmake-build-debug/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src"
+  "/Users/mherbert/Desktop/pico-w/cmake-build-debug/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src/PioasmBuild-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/mherbert/Desktop/pico-w/cmake-build-debug/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src/PioasmBuild-stamp/${subDir}")
+endforeach()
